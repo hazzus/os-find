2a87cd156760afc0842ae47e2e7960b7ace21c47
hazzus <dfa.sro591@gmail.com>
2019-04-18 10:27:36 +0300

    common identation

--- exec_utils.hpp
+++ exec_utils.hpp
@@ -11,44 +11,44 @@
 #include <unistd.h>
 
 void error(std::string const &cause) {
-  std::cerr << cause << strerror(errno) << std::endl;
+    std::cerr << cause << strerror(errno) << std::endl;
 }
 
 char *converter(std::string const &s) { return const_cast<char *>(s.data()); }
 
 std::vector<char *> get_ptrs(std::vector<std::string> &args) {
-  std::vector<char *> result;
-  std::transform(args.begin(), args.end(), std::back_inserter(result),
-                 converter);
-  return result;
+    std::vector<char *> result;
+    std::transform(args.begin(), args.end(), std::back_inserter(result),
+                   converter);
+    return result;
 }
 
 void execute(char *argv[]) {
-  switch (auto pid = fork()) {
-  case -1:
-    error("Can not fork");
-    break;
-  case 0:
-    if (execv(argv[0], argv) == -1) {
-      error("Execution failed");
-      exit(-1);
+    switch (auto pid = fork()) {
+    case -1:
+        error("Can not fork");
+        break;
+    case 0:
+        if (execv(argv[0], argv) == -1) {
+            error("Execution failed");
+            exit(-1);
+        }
+        exit(0);
+    default:
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            error("Error in execution");
+        } else {
+            std::cout << "Executed. Return code: " << WEXITSTATUS(status)
+                      << std::endl;
+        }
     }
-    exit(0);
-  default:
-    int status;
-    if (waitpid(pid, &status, 0) == -1) {
-      error("Error in execution");
-    } else {
-      std::cout << "Executed. Return code: " << WEXITSTATUS(status)
-                << std::endl;
-    }
-  }
 }
 
 void pre_execute(std::vector<std::string> &args) {
-  auto prepared = get_ptrs(args);
-  prepared.push_back(nullptr);
-  execute(prepared.data());
+    auto prepared = get_ptrs(args);
+    prepared.push_back(nullptr);
+    execute(prepared.data());
 }
 
 #endif
--- parse_utils.hpp
+++ parse_utils.hpp
@@ -22,50 +22,50 @@ static std::map<std::string, std::string> options = {
 // clang-format on
 
 std::pair<std::string, std::string> parse_arg(char *arg) {
-  std::string arg_str = arg;
-  for (size_t i = 0; i < arg_str.length(); i++) {
-    if (arg_str[i] == '=') {
-      std::string value = arg_str.substr(i + 1);
-      if (value.front() == value.back() &&
-          (value.front() == '\'' || value.front() == '\"'))
-        value = value.substr(1, value.length() - 2);
-      return {arg_str.substr(1, i - 1), value};
+    std::string arg_str = arg;
+    for (size_t i = 0; i < arg_str.length(); i++) {
+        if (arg_str[i] == '=') {
+            std::string value = arg_str.substr(i + 1);
+            if (value.front() == value.back() &&
+                (value.front() == '\'' || value.front() == '\"'))
+                value = value.substr(1, value.length() - 2);
+            return {arg_str.substr(1, i - 1), value};
+        }
     }
-  }
-  return {arg_str.substr(1), ""};
+    return {arg_str.substr(1), ""};
 }
 
 std::map<std::string, std::string> parse(int const &argc, char *argv[]) {
-  std::map<std::string, std::string> result;
-  std::string first_arg = argv[1];
-  if (first_arg == "-help") {
-    result.emplace("help", "");
-  } else {
-    result.emplace("path", std::string(argv[1]));
-  }
-  for (int i = 2; i < argc; i++) {
-    result.insert(parse_arg(argv[i]));
-  }
-  return result;
+    std::map<std::string, std::string> result;
+    std::string first_arg = argv[1];
+    if (first_arg == "-help") {
+        result.emplace("help", "");
+    } else {
+        result.emplace("path", std::string(argv[1]));
+    }
+    for (int i = 2; i < argc; i++) {
+        result.insert(parse_arg(argv[i]));
+    }
+    return result;
 }
 
 bool check(std::map<std::string, std::string> const &args) {
-  for (auto &&arg_pair : args) {
-    if (!options.count(arg_pair.first)) {
-      std::cerr << "Unexpected key: -" << arg_pair.first << std::endl;
-      return false;
+    for (auto &&arg_pair : args) {
+        if (!options.count(arg_pair.first)) {
+            std::cerr << "Unexpected key: -" << arg_pair.first << std::endl;
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 std::string help() {
-  std::string result = std::accumulate(
-      options.begin(), options.end(), std::string("Options:\n"),
-      [](std::string a, std::pair<std::string, std::string> p) {
-        return std::move(a) + "-" + p.first + ": " + p.second + "\n";
-      });
-  return result;
+    std::string result = std::accumulate(
+        options.begin(), options.end(), std::string("Options:\n"),
+        [](std::string a, std::pair<std::string, std::string> p) {
+            return std::move(a) + "-" + p.first + ": " + p.second + "\n";
+        });
+    return result;
 }
 
 } // namespace args
